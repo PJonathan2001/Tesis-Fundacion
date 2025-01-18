bbece29dbd284496051d542616f5afe37aaa562f
PJonathan2001 <87325282+PJonathan2001@users.noreply.github.com>
2025-01-18 18:56:18 -0500

    pruebadevsecops

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,3 +4,4 @@ import authConfig from "../../../../config/auth/config";
 export const { signIn, signOut, auth } = NextAuth(authConfig);
 
 // Exporta explícitamente los métodos HTTP GET y POST
+//prueba de devsecops
